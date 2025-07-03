a46b8135edde165ac624f03d13a9ae963df7c2d4
Ravi Kumar <ravi.kumhar@outlook.com>
2025-07-03 11:33:27 +0530

    Fix Metro config for TypeScript support

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,9 @@ const config = getDefaultConfig(__dirname);
 // Add 'ts' and 'tsx' to the list of source extensions
 config.resolver.sourceExts.push('ts', 'tsx');
 
+// Set the Babel transformer path to handle TypeScript files properly
+config.transformer.babelTransformerPath = require.resolve('metro-react-native-babel-transformer');
+
 // Ensure that the transformer processes all files, including those in node_modules
 config.transformer.getTransformOptions = async () => ({
   transform: {
